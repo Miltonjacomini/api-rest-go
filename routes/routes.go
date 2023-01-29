692e3d3bf93283196482ef6ed56d27296de1346e
Milton Giacomini Neto <milton.jneto@mercadolivre.com>
2023-01-29 11:56:05 -0300

    Adiciona rota para personalidades
    - Adiciona gorilla_mux ao projeto para facilitar
    a manipulação das rotas.
    - Adiciona endpoint para retornar uma personalidade
    específica por ID.
    - Altera o modelo Personalidades para incluir ID.

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/miltonjacomini/go-rest-api/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.Personalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.Personalidades)
+	r.HandleFunc("/api/personalidades/{id}", controllers.PersonalidadePorId)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
